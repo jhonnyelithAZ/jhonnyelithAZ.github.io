d0497aac9ca04f9cbc5532b0bac7235c66a40143
jhonatan ayala <jhonatanayala3478@gmail.com>
2026-04-22 01:37:17 -0500

    docs: update README with demo gif and final sections

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const botonMenu = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav-principal');
 const links = document.querySelectorAll('.nav-principal a');
 
+
 // Cuando dan clic a la hamburguesa
 botonMenu.addEventListener('click', () => {
   nav.classList.toggle('activo'); // Muestra/oculta el menú
@@ -16,13 +17,33 @@ links.forEach(link => {
     botonMenu.classList.remove('activo');
   });
 });
-
-// --- COFRE DE PROYECTOS ---
+// --- COFRE DE PROYECTOS CON SONIDO ---
 const cofre = document.querySelector('.cofre-proyectos');
 const contenidoCofre = document.querySelector('.contenido-cofre');
+const imagenCofre = document.querySelector('.icono-cofre');
+
+// Agarra los audios del DOM
+const sonidoAbrir = document.querySelector('#sonido-abrir');
+const sonidoCerrar = document.querySelector('#sonido-cerrar');
 
 cofre.addEventListener('click', () => {
-  // Alterna la clase 'abierto' en ambos elementos
+  const estaAbierto = cofre.classList.contains('abierto');
+  
+  // Alterna las clases para CSS
   cofre.classList.toggle('abierto');
   contenidoCofre.classList.toggle('abierto');
+  
+  if (!estaAbierto) {
+    // Si estaba cerrado y ahora se abre
+    imagenCofre.src = 'public/img/cofre-abierto.png';
+    imagenCofre.alt = 'Cofre de proyectos abierto';
+    sonidoAbrir.currentTime = 0; // Rebobina por si se spamea el clic
+    sonidoAbrir.play();
+  } else {
+    // Si estaba abierto y ahora se cierra
+    imagenCofre.src = 'public/img/cofre-cerrado.png';
+    imagenCofre.alt = 'Cofre de proyectos cerrado';
+    sonidoCerrar.currentTime = 0;
+    sonidoCerrar.play();
+  }
 });
