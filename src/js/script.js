396158ea7c0cc1bbf0dd1e328bf86802becebe4e
jhonatan ayala <jhonatanayala3478@gmail.com>
2026-04-24 15:47:02 -0500

    finishim

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,10 @@ const nav = document.querySelector('.nav-principal');
 const links = document.querySelectorAll('.nav-principal a');
 
 
-
+/*part of the hamburger menu*/
 botonMenu.addEventListener('click', () => {
-  nav.classList.toggle('activo'); 
-  botonMenu.classList.toggle('activo'); 
+  nav.classList.toggle('activo');
+  botonMenu.classList.toggle('activo');
 });
 
 
@@ -26,18 +26,19 @@ const imagenCofre = document.querySelector('.icono-cofre');
 const sonidoAbrir = document.querySelector('#sonido-abrir');
 const sonidoCerrar = document.querySelector('#sonido-cerrar');
 
+/*part of the chest*/
 cofre.addEventListener('click', () => {
   const estaAbierto = cofre.classList.contains('abierto');
-  
+
 
   cofre.classList.toggle('abierto');
   contenidoCofre.classList.toggle('abierto');
-  
+
   if (!estaAbierto) {
-  
+
     imagenCofre.src = 'public/img/cofre-abierto.png';
     imagenCofre.alt = 'Cofre de proyectos abierto';
-    sonidoAbrir.currentTime = 0; 
+    sonidoAbrir.currentTime = 0;
     sonidoAbrir.play();
   } else {
 
